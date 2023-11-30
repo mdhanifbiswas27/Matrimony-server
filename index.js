74419d35f4a119445b357e7bf276c601f8bff19b
mdhanifbiswas27 <mdhanifbiswas27@gmail.com>
2023-11-30 19:00:14 +0600

    biodata related api are completed

--- index.js
+++ index.js
@@ -30,10 +30,23 @@ async function run() {
 
     const userCollection = client.db("assigment-twelveDb").collection("user");
     const reviewCollection = client.db("assigment-twelveDb").collection("review");
+    const biodataCollection = client.db("assigment-twelveDb").collection("biodata");
 
     // get all premium user
   app.get('/user', async(req, res)=>{
     const result = await userCollection.find().toArray();
+    res.send(result);
+  })
+  // get all bio data
+  app.get('/biodata', async(req, res)=>{
+    const result = await biodataCollection.find().toArray();
+    res.send(result);
+  })
+  // get bio data by id
+  app.get('/biodata/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await biodataCollection.find(query).toArray();
     res.send(result);
   })
 
